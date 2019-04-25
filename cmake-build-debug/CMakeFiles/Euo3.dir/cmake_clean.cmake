6234a392083ea1bda9ea20324555c92a3653b142
tokeron <49562866+tokeron@users.noreply.github.com>
2019-04-25 17:34:25 +0300

    new

---
+++ cmake-build-debug/CMakeFiles/Euo3.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Euo3.dir/map.c.obj"
+  "CMakeFiles/Euo3.dir/euo.c.obj"
+  "Euo3.pdb"
+  "Euo3.exe"
+  "Euo3.exe.manifest"
+  "libEuo3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Euo3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
